4d1214a44df942cf83cdaa881860f0f24e7260fd
hilmyrs123 <hilmyrs123@gmail.com>
2023-02-20 17:10:48 +0700

    3rd impact

--- java/Main.java
+++ java/Main.java
@@ -7,7 +7,7 @@ public class Main{
     public static void printmenu(){
         System.out.println("-----------------");
         System.out.println("Ketik 0 untuk exit");
-        System.out.println("Mau apa? Ketik di sini: ");
+        System.out.println("Ketik angka di sini untuk menambah data: ");
     }
 
     public static void printlist(ArrayList<Sivitas> list){
@@ -30,7 +30,7 @@ public class Main{
 
         int i = 0;
         
-        //Instansiasi dan mencoba masukin satu data dengan add method
+        //Instansiasi pake sivitas krn biar bawa semua atribut ortu
         Sivitas civitas = new Sivitas();
         civitas.setnama("Ujang");
         civitas.setnik("322");
@@ -57,7 +57,7 @@ public class Main{
         while(valid != false){
             wantedmethod = sc.nextInt();
             if(wantedmethod >= 1 && wantedmethod != 0){
-                //instantiasi mhsnew
+                //instantiasi mhsnew dari sivitas agar bisa bawa atribut ortunya
                 Sivitas mhsnew = new Sivitas();
 
                 //untuk makan new line dari si nextInt()
@@ -67,12 +67,20 @@ public class Main{
 
                 System.out.print("Input Nama : "); 
                 mhsnew.setnama(sc.nextLine());
+                System.out.print("Input NIK : ");
+                mhsnew.setnik(sc.nextLine());
+                System.out.print("Input Gender ");
+                mhsnew.setgender(sc.nextLine());
                 System.out.print("Input NIM : ");
                 mhsnew.setNIM(sc.nextLine());
                 System.out.print("Input Prodi : ");
                 mhsnew.setprodi(sc.nextLine());
                 System.out.print("Input Fakultas : ");
                 mhsnew.setfakultas(sc.nextLine());
+                System.out.print("Input Asal Universitas : ");
+                mhsnew.setasal(sc.nextLine());
+                System.out.print("Input Email : ");
+                mhsnew.setemail(sc.nextLine());
 
                 list.add(mhsnew);
 
